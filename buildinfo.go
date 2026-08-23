7964b4a96d81a6938edb29f25b7f16615e81bc37
Tejas A <tejasa97@users.noreply.github.com>
2026-08-23 17:02:49 +0530

    chore: pin hardened browser-cookie engine

--- buildinfo.go
+++ buildinfo.go
@@ -12,7 +12,7 @@ var diagnosticsEventsEndpoint = "https://diagnostics.vidstow.workers.dev/v1/even
 const (
 	appVersion          = "0.1.0-beta.5"
 	engineModulePath    = "github.com/tejasa97/ytdlp-go"
-	pinnedEngineVersion = "v0.3.1-0.20260823095847-72483a2e2328"
+	pinnedEngineVersion = "v0.3.1-0.20260823104059-178695c8b2be"
 )
 
 // BuildInfo is the single backend-owned release identity exposed to the UI
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.25.12
 
 require (
 	github.com/google/uuid v1.6.0
-	github.com/tejasa97/ytdlp-go v0.3.1-0.20260823095847-72483a2e2328
+	github.com/tejasa97/ytdlp-go v0.3.1-0.20260823104059-178695c8b2be
 	github.com/wailsapp/wails/v2 v2.13.0
 	golang.org/x/sys v0.45.0
 	golang.org/x/text v0.37.0
